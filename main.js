25d621e2cdff6645f94ee5588a68df98f6eacc39
Jv Shah <88554440+Jvshah124@users.noreply.git>
2023-12-08 19:42:50 +0530

    few changes

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const sr = ScrollReveal({
   reset: true,
 });
 
-// sr.reveal(".logo", { delay: 300, origin: "top" });
+sr.reveal(".logo", { delay: 300, origin: "top" });
 // sr.reveal(".navbar", { delay: 300, origin: "top" });
 sr.reveal(".right img", { delay: 1000, origin: "right" });
 sr.reveal(".left", { delay: 1000, origin: "left" });
