134156fd90289bc5f967dfb592d9ef81070e48c7
Jv Shah <88554440+Jvshah124@users.noreply.git>
2023-12-13 12:33:33 +0530

    added skills section

--- main.js
+++ main.js
@@ -29,3 +29,36 @@ sr.reveal(".logo", { delay: 300, origin: "top" });
 sr.reveal(".right img", { delay: 1000, origin: "right" });
 sr.reveal(".left", { delay: 1000, origin: "left" });
 sr.reveal(".about", { delay: 250, origin: "bottom" });
+
+// Google Sheets Contact Form Script
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbx0hygDrdmo1y1_bh3RK2Vb6pLGn5UTmAJcoIg0XU-X9vibrY7g8tj0QgAFHzwiBSmQWw/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message Sent Succesfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 3000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
+// GLOWING CARD On HOVER ANIMATION
+let cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.onmousemove = function (e) {
+    let x = e.pageX - card.offsetLeft;
+    let y = e.pageY - card.offsetTop;
+
+    card.style.setProperty("--x", x + "px");
+    card.style.setProperty("--y", y + "px");
+  };
+});
